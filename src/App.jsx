1e43901b4cb055b1239009b1ccb1e0f0b65696da
Chris Umali <chrisumali.work@gmail.com>
2026-04-14 22:58:01 +0800

    Rendernig contents conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 /*All imports that starts with 'use' are react hooks
 all react hooks must be called only inside react components or other react hooks*/
 
-import { useState } from 'react';
+import { useState } from "react";
 
-import  Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import { CORE_CONCEPTS, EXAMPLES } from './data.js';
-import TabButton from './components/TabButton.jsx';
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
   //Hook useState called on top level
-  const [selectedTopic, setSelectedTopic ] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
 
-  function handleSelect(selectedButton){
+  function handleSelect(selectedButton) {
     //selectedButton => 'components', 'jsx', 'props', 'state'
     setSelectedTopic(selectedButton);
     console.log(selectedTopic);
@@ -22,30 +22,40 @@ function App() {
     <div>
       <Header />
       <main>
-        <section id='core-concepts'>
+        <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
             <CoreConcept {...CORE_CONCEPTS[0]} />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image} />
+            <CoreConcept
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
+            />
           </ul>
         </section>
-        <section id='examples'>
+        <section id="examples">
           <h2>Example</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Component</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Component
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          <div id='tab-content'>
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {!selectedTopic ? (
+            <p>Please select a topic</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
@@ -55,7 +65,7 @@ function App() {
 export default App;
 
 /*
-* Rules of hooks:
-* Only call hooks inside of component functions
-* Only call hooks on the top level
-*/
+ * Rules of hooks:
+ * Only call hooks inside of component functions
+ * Only call hooks on the top level
+ */
