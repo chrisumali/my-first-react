9d292d9dd97b388e03dd957b7f77fcb1cfa2c1f0
Chris Umali <chrisumali.work@gmail.com>
2026-04-15 12:01:37 +0800

    Output list data dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -25,25 +25,38 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image}
-            />
+            {CORE_CONCEPTS.map((item) => (
+              <CoreConcept key={item.title} {...item} />
+            ))}
           </ul>
         </section>
         <section id="examples">
           <h2>Example</h2>
           <menu>
-            <TabButton isSelected={selectedTopic == 'components'} onSelect={() => handleSelect("components")}>
+            <TabButton
+              isSelected={selectedTopic == "components"}
+              onSelect={() => handleSelect("components")}
+            >
               Component
             </TabButton>
-            <TabButton isSelected={selectedTopic == 'jsx'}  onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic == 'props'}  onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton isSelected={selectedTopic == 'state'}  onSelect={() => handleSelect("state")}>State</TabButton>
+            <TabButton
+              isSelected={selectedTopic == "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic == "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic == "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
           </menu>
           {!selectedTopic ? (
             <p>Please select a topic</p>
